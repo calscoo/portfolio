11f2586024c80061fe5d84b9e3873c18300567ba
calscoo <scoutscott13@gmail.com>
2024-08-31 14:57:12 -0700

    gha

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,21 +1,13 @@
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = "us-east-2"
+  region = "us-east-2"
 }
 
-variable "aws_access_key" {
-  description = "AWS Personal Access Key"
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Personal Secret Key"
-  sensitive   = true
-}
-
-variable "github_access_token" {
-  description = "GitHub Access Token"
-  sensitive   = true
+terraform {
+  backend "s3" {
+    bucket  = "calscoo-terraform-state"
+    key     = "portfolio.tfstate"
+    encrypt = true
+    region  = "us-east-2"
+  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@
 resource "aws_amplify_app" "portfolio" {
   name       = "portfolio"
   repository = "https://github.com/calscoo/portfolio"
-  access_token = var.github_access_token
+  # access_token = var.github_access_token
 
   # The default build_spec added by the Amplify Console for React.
   build_spec = <<-EOT
@@ -23,15 +23,21 @@ applications:
   EOT
 
   custom_rule {
+    source = "https://calebolson.space"
+    status = "302"
+    target = "https://www.calebolson.space"
+  }
+
+  custom_rule {
     source = "/<*>"
     status = "404"
     target = "/index.html"
   }
 
   environment_variables = {
-    AMPLIFY_DIFF_DEPLOY = "false"
+    AMPLIFY_DIFF_DEPLOY       = "false"
     AMPLIFY_MONOREPO_APP_ROOT = "hugo"
-    _LIVE_UPDATES = "[{\"name\":\"Hugo\",\"pkg\":\"hugo\",\"type\":\"hugo\",\"version\":\"latest\"}]"
+    _LIVE_UPDATES             = "[{\"name\":\"Hugo\",\"pkg\":\"hugo\",\"type\":\"hugo\",\"version\":\"latest\"}]"
   }
 }
 
@@ -44,4 +50,23 @@ resource "aws_amplify_webhook" "main_webhook" {
   app_id      = aws_amplify_app.portfolio.id
   branch_name = aws_amplify_branch.main_branch.branch_name
   description = "triggermain"
+}
+
+resource "aws_amplify_domain_association" "domain" {
+  app_id                = aws_amplify_app.portfolio.id
+  wait_for_verification = true
+  domain_name           = "calebolson.space"
+  certificate_settings {
+    type = "AMPLIFY_MANAGED"
+  }
+
+  sub_domain {
+    branch_name = aws_amplify_branch.main_branch.branch_name
+    prefix      = ""
+  }
+
+  sub_domain {
+    branch_name = aws_amplify_branch.main_branch.branch_name
+    prefix      = "www"
+  }
 }
